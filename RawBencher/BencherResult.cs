1da245e0fbb396b3ee233cb258a2413118b0b0ba
Solutions Design bv <frans@sd.nl>
2013-12-18 19:20:13 +0100

    Revert "Removed unnecessary class BencherResult"
    This reverts commit f5b87a43ceaff97ecf8fdc79099eef695c31f311.

---
+++ RawBencher/BencherResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RawBencher
+{
+	/// <summary>
+	/// Contains the results of a bencher after all tests have been run.
+	/// </summary>
+	public class BencherResult
+	{
+		#region Properties.
+
+		#endregion
+	}
+}
