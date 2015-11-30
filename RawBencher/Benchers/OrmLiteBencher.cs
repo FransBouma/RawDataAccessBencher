cb2fb388a5382fb9356bfd60efc95961edd74196
Demis Bellot <demis.bellot@gmail.com>
2015-11-30 00:37:42 -0800

    Update formatting

--- RawBencher/Benchers/OrmLiteBencher.cs
+++ RawBencher/Benchers/OrmLiteBencher.cs
@@ -6,16 +6,17 @@ using ServiceStack.OrmLite;
 
 namespace RawBencher.Benchers
 {
-	/// <summary>
-	/// Specific bencher for OrmLite , doing no-change tracking fetch
-	/// </summary>
-	public class OrmLiteBencher : BencherBase<SalesOrderHeader>
-	{
+    /// <summary>
+    /// Specific bencher for OrmLite, doing no-change tracking fetch
+    /// </summary>
+    public class OrmLiteBencher : BencherBase<SalesOrderHeader>
+    {
         /// <summary>
         /// Initializes a new instance of the <see cref="OrmLiteBencher"/> class.
         /// </summary>
         public OrmLiteBencher()
-			: base(e => e.SalesOrderId, usesChangeTracking:false, usesCaching:false) {}
+            : base(e => e.SalesOrderId, usesChangeTracking: false, usesCaching: false)
+        { }
 
         /// <summary>
         /// Fetches the individual element
@@ -23,7 +24,7 @@ namespace RawBencher.Benchers
         /// <param name="key">The key of the element to fetch.</param>
         /// <returns>The fetched element, or null if not found</returns>
         public override SalesOrderHeader FetchIndividual(int key)
-		{
+        {
             SalesOrderHeader toReturn = null;
             using (var con = dbFactory.OpenDbConnection())
             {
@@ -33,33 +34,30 @@ namespace RawBencher.Benchers
             return toReturn;
         }
 
+        /// <summary>
+        /// Fetches the complete set of elements and returns this set as an IEnumerable.
+        /// </summary>
+        /// <returns>the set fetched</returns>
+        public override IEnumerable<SalesOrderHeader> FetchSet()
+        {
+            var headers = new List<SalesOrderHeader>();
+            using (var con = dbFactory.OpenDbConnection())
+            {
+                headers = con.SqlList<SalesOrderHeader>(this.CommandText);
+                con.Close();
+            }
+            return headers;
+        }
 
-		/// <summary>
-		/// Fetches the complete set of elements and returns this set as an IEnumerable.
-		/// </summary>
-		/// <returns>the set fetched</returns>
-		public override IEnumerable<SalesOrderHeader> FetchSet()
-		{
-			var headers = new List<SalesOrderHeader>();
-			using(var con = dbFactory.OpenDbConnection())
-			{
-				headers = con.SqlList<SalesOrderHeader>(this.CommandText);
-				con.Close();
-			}
-			return headers;
-		}
-
-
-		/// <summary>
-		/// Creates the name of the framework this bencher is for. Use the overload which accepts a format string and a type to create a name based on a
-		/// specific version
-		/// </summary>
-		/// <returns>the framework name.</returns>
-		protected override string CreateFrameworkNameImpl()
-		{
-			return CreateFrameworkName("ServiceStack OrmLite v{0} (v{1})", typeof(OrmLiteConnectionFactory));
-		}
-
+        /// <summary>
+        /// Creates the name of the framework this bencher is for. Use the overload which accepts a format string and a type to create a name based on a
+        /// specific version
+        /// </summary>
+        /// <returns>the framework name.</returns>
+        protected override string CreateFrameworkNameImpl()
+        {
+            return CreateFrameworkName("ServiceStack OrmLite v{0} (v{1})", typeof(OrmLiteConnectionFactory));
+        }
 
         #region Properties
         /// <summary>
@@ -68,7 +66,7 @@ namespace RawBencher.Benchers
         IDbConnectionFactory dbFactory;
         public string ConnectionStringToUse
         {
-		    set { dbFactory = new OrmLiteConnectionFactory(value, SqlServerDialect.Provider); }
+            set { dbFactory = new OrmLiteConnectionFactory(value, SqlServerDialect.Provider); }
         }
 
         public string CommandText { get; set; }
